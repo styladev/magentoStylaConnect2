1f087e456068cc758d04f8a88997532114950496
Oskar <owolanin@divante.pl>
2018-12-17 13:37:12 +0100

    fixed rootPath for magazine content

--- Block/Magazine/Content.php
+++ Block/Magazine/Content.php
@@ -7,20 +7,27 @@ use Styla\Connect2\Helper\Data;
 use Magento\Framework\Registry;
 use Magento\Framework\View\Element\Template;
 use Styla\Connect2\Helper\Config;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Content extends Magazine
 {
     protected $helper;
+
+    protected $rootPath;
+
+    protected $storeManager;
 
     public function __construct(
         Template\Context $context,
         Registry $registry,
         Config $configHelper,
         Data $helper,
+        StoreManagerInterface $storeManager,
         array $data = []
     ) {
+        $this->storeManager = $storeManager;
         $this->helper = $helper;
-        parent::__construct($context, $registry ,$helper, $data);
+        parent::__construct($context, $registry, $helper, $data);
     }
 
     /**
@@ -39,7 +46,23 @@ class Content extends Magazine
      */
     public function getRootPath()
     {
-        return $this->helper->getCurrentMagazine();
+        if (null === $this->rootPath) {
+            $routeName = trim($this->helper->getCurrentMagazine()->getFrontName(), '/') . '/';
+
+            $url = parse_url(
+                str_replace(
+                    '/index.php/',
+                    '/',
+                    $this->storeManager->getStore()->getUrl(
+                        $routeName,
+                        ['_type' => \Magento\Framework\UrlInterface::URL_TYPE_LINK]
+                    )
+                )
+            );
+            $this->rootPath = isset($url['path']) ? $url['path'] : '';
+        }
+
+        return $this->rootPath;
     }
 
     /**
