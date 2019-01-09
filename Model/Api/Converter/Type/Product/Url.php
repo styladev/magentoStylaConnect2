b69868458fcb07a4536dcf516e0876266edd2cd8
Oskar <owolanin@divante.pl>
2019-01-09 11:56:26 +0100

    added 'relatative product urls' usability

--- Model/Api/Converter/Type/Product/Url.php
+++ Model/Api/Converter/Type/Product/Url.php
@@ -2,13 +2,44 @@
 namespace Styla\Connect2\Model\Api\Converter\Type\Product;
 
 use Styla\Connect2\Model\Api\Converter\Type as ConverterType;
+use Styla\Connect2\Helper\Data as StylaHelper;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Url extends ConverterType\AbstractType
 {
+    protected $stylaHelper;
+
+    protected $storeManager;
+
+    public function __construct(
+        StylaHelper $helper,
+        StoreManagerInterface $storeManager)
+    {
+        $this->stylaHelper = $helper;
+        $this->storeManager = $storeManager;
+    }
+
     protected function _convertItem($item)
     {
         //get the public product url
         $this->_convertedValue = $item->getProductUrl();
+        if ($this->useRelativeUrls()) {
+            $this->_convertedValue = str_replace(
+                $this->storeManager->getStore()->getBaseUrl(),
+                '/',
+                $item->getProductUrl()
+            );
+        }
+    }
+
+    /**
+     * Should only return the relative part of the urls
+     *
+     * @return bool
+     */
+    protected function useRelativeUrls()
+    {
+        return $this->stylaHelper->isUsingRelativeProductUrls();
     }
 }
 
