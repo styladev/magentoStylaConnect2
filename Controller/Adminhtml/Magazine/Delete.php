495a27cd7cac8466035a67373e02e1ba6052976f
Oskar <owolanin@divante.pl>
2019-02-01 09:00:08 +0100

    cr fixes

--- Controller/Adminhtml/Magazine/Delete.php
+++ Controller/Adminhtml/Magazine/Delete.php
@@ -10,28 +10,16 @@ namespace Styla\Connect2\Controller\Adminhtml\Magazine;
 
 use \Magento\Backend\App\Action;
 use \Magento\Backend\App\Action\Context;
-use \Magento\Framework\View\Result\PageFactory;
 use \Styla\Connect2\Model\MagazineFactory;
 use \Magento\Framework\Controller\ResultFactory;
-use \Magento\Framework\Registry;
 use \Magento\Framework\App\Cache\Manager;
 
 class Delete extends Action
 {
     /**
-     * @var bool|PageFactory
-     */
-    protected $resultPageFactory;
-
-    /**
      * @var MagazineFactory
      */
     protected $magazineFactory;
-
-    /**
-     * @var Registry
-     */
-    protected $coreRegistry;
 
     /**
      * @var Manager
@@ -42,27 +30,20 @@ class Delete extends Action
      * Add constructor.
      *
      * @param Context $context
-     * @param PageFactory $resultPageFactory
      * @param MagazineFactory $magazineFactory
-     * @param Registry $coreRegistry
      * @param Manager $cacheManager
      *
      * @return void
      */
     public function __construct(
         Context $context,
-        PageFactory $resultPageFactory,
         MagazineFactory $magazineFactory,
-        Registry $coreRegistry,
         Manager $cacheManager
     )
     {
         parent::__construct($context);
-        $this->resultPageFactory = $resultPageFactory;
         $this->magazineFactory = $magazineFactory;
-        $this->messageManager = $context->getMessageManager();
         $this->resultFactory = $context->getResultFactory();
-        $this->coreRegistry = $coreRegistry;
         $this->cacheManager = $cacheManager;
     }
 
@@ -84,26 +65,5 @@ class Delete extends Action
         }
 
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
-    }
-
-    /**
-     * @param $magazineData
-     *
-     * @return array
-     */
-    private function filterSaveData(&$magazineData)
-    {
-        if (isset($magazineData['form_key'])) {
-            unset($magazineData['form_key']);
-        }
-
-        if (isset($magazineData['id_field_name'])) {
-            unset($magazineData['id_field_name']);
-        }
-
-        $magazineData['front_name'] = preg_replace('!\s+!', ' ', $magazineData['front_name']);
-        $magazineData['front_name'] = str_replace(' ', '-', $magazineData['front_name']);
-
-        return $magazineData;
     }
 }
